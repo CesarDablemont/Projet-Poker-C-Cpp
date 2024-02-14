c143e8bb85c44e303ecfec866375c880bcd56410
Cesar Dablemont <cesardablemont@gmail.com>
2024-02-14 10:30:01 +0100

    add a little test code

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 int main()
 {
     std::cout << "Run ...." << std::endl;
-    
-    Card c1 = Card(2, 1); // 2 de coeur
-    std::cout << "value: " << c1.get_value() << std::endl;
-    std::cout << "color: " << c1.get_color() << std::endl;
+
+
+    std::cout << "2 de coeur" << std::endl;
+    Card c1 = Card(Card::DEUX, Card::COEUR); // 2 de coeur
+    std::cout << "value: " << c1.get_value() << ", color: " << c1.get_color() << std::endl;
+    std::cout << "value: " << c1.get_value() << ", color: "<< c1.colors[c1.get_color()] << std::endl;
 
     std::cout << "This is the end, just ..." << std::endl;
 }
