39e44919b3679b0ba0c6bd2ec91fdff1011b0434
Cesar Dablemont <cesardablemont@gmail.com>
2024-02-26 18:01:46 +0100

    add has_amount_of_card function and some minor changes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <algorithm>
 #include <iostream>
-#include <iterator>
+#include <unordered_map>
 #include <vector>
 
 #include "card.h"
@@ -29,6 +29,19 @@ void display_hand(std::vector<Card *> hand) {
   std::cout << std::endl;
 }
 
+void display_unordered_map(const std::unordered_map<Card::VALUE, int> &uMap) {
+  if (uMap.empty()) {
+    std::cout << "Empty unordered_map" << std::endl;
+    return;
+  }
+
+  for (const auto &pair : uMap)
+    std::cout << "key " << static_cast<int>(pair.first) << ": value "
+              << pair.second << ", ";
+
+  std::cout << std::endl;
+}
+
 #pragma endregion
 
 /* ***************************************************************************
@@ -40,7 +53,24 @@ bool compareByValue(const Card *card1, const Card *card2) {
   return card1->get_value() < card2->get_value();
 }
 
-bool has_amount_of_card(std::vector<Card *> hand, int number) {}
+bool has_amount_of_card(std::vector<Card *> hand, int number) {
+  // std::sort(hand.begin(), hand.end(), compareByValue);
+
+  // Une map poure le nombre de fréquences de chaque valeur dans le vecteur
+  std::unordered_map<Card::VALUE, int> freqMap;
+
+  for (int i = 0; i < 5; i++) {
+    auto value = hand[i]->get_value();
+    Card::VALUE cardValue = static_cast<Card::VALUE>(value);
+    freqMap[cardValue]++;
+  }
+
+  // display_unordered_map(freqMap);
+
+  return std::any_of(
+      freqMap.begin(), freqMap.end(),
+      [number](const auto &pair) { return pair.second == number; });
+}
 
 bool color(std::vector<Card *> hand) {
   if (hand.empty()) return false;
@@ -72,7 +102,25 @@ bool sequence(std::vector<Card *> &hand) {
   return true;
 }
 
-int best_hand(std::vector<Card *> hand) { return 0; }
+int best_hand(std::vector<Card *> hand) {
+  std::sort(hand.begin(), hand.end(), compareByValue);
+
+  if (color(hand) && sequence(hand)) return 100000000;
+  if (has_amount_of_card(hand, 4)) return 90000000;
+  if (has_amount_of_card(hand, 3) && has_amount_of_card(hand, 2))
+    return 80000000;
+  if (color(hand)) return 70000000;
+  if (sequence(hand)) return 60000000;
+  if (has_amount_of_card(hand, 3)) return 50000000;
+
+  // double paire return 40000000;
+
+  if (has_amount_of_card(hand, 2)) return 30000000;
+
+  // hauteur return 20000000;
+
+  return 0;
+}
 
 #pragma endregion
 
@@ -81,7 +129,7 @@ int best_hand(std::vector<Card *> hand) { return 0; }
 *************************************************************************** */
 #pragma region
 
-void test_best_hand() {
+void test_best_hand_sub_function() {
   std::vector<Card *> hand;
   hand.push_back(new Card(Card::UN, Card::COEUR));
   hand.push_back(new Card(Card::DEUX, Card::COEUR));
@@ -103,7 +151,21 @@ void test_best_hand() {
   hand3.push_back(new Card(Card::QUATRE, Card::COEUR));
   hand3.push_back(new Card(Card::TROIS, Card::PIQUE));
 
-  std::cout << "Debut des test." << std::endl;
+  std::vector<Card *> hand4;
+  hand4.push_back(new Card(Card::ROI, Card::PIQUE));
+  hand4.push_back(new Card(Card::ROI, Card::TREFLE));
+  hand4.push_back(new Card(Card::ROI, Card::CARREAU));
+  hand4.push_back(new Card(Card::DAME, Card::COEUR));
+  hand4.push_back(new Card(Card::DAME, Card::PIQUE));
+
+  std::vector<Card *> hand5;
+  hand5.push_back(new Card(Card::ROI, Card::PIQUE));
+  hand5.push_back(new Card(Card::ROI, Card::TREFLE));
+  hand5.push_back(new Card(Card::ROI, Card::CARREAU));
+  hand5.push_back(new Card(Card::ROI, Card::COEUR));
+  hand5.push_back(new Card(Card::DAME, Card::PIQUE));
+
+  std::cout << "Debut des test (test_best_hand_sub_function)." << std::endl;
 
   if (!color(hand) == true)
     std::cout << "Echec du test de la 1e couleur." << std::endl;
@@ -117,13 +179,44 @@ void test_best_hand() {
   if (!sequence(hand3) == true)
     std::cout << "Echec du test de la 3e suite." << std::endl;
 
-  if (!has_amount_of_card(hand2, 2) == false)
-    std::cout << "Echec du test de la paire." << std::endl;
+  if (!has_amount_of_card(hand2, 2) == true)
+    std::cout << "Echec du 1e test de la paire." << std::endl;
+  if (!has_amount_of_card(hand, 2) == false)
+    std::cout << "Echec du 2e test de la paire." << std::endl;
+
+  if (!has_amount_of_card(hand4, 3) == true)
+    std::cout << "Echec du 1e test du brelan." << std::endl;
+  if (!has_amount_of_card(hand2, 3) == false)
+    std::cout << "Echec du 2e test du brelan." << std::endl;
+
+  if (!has_amount_of_card(hand5, 4) == true)
+    std::cout << "Echec du 1e test du carrE." << std::endl;
+  if (!has_amount_of_card(hand4, 4) == false)
+    std::cout << "Echec du 2e test du carrE." << std::endl;
 
   for (Card *i : hand) delete i;
   for (Card *i : hand2) delete i;
   for (Card *i : hand3) delete i;
+  for (Card *i : hand4) delete i;
+  for (Card *i : hand5) delete i;
 
+  std::cout << "Tout les test ont etais passe" << std::endl;
+}
+
+void test_best_hand() {
+  std::vector<Card *> hand;
+  hand.push_back(new Card(Card::UN, Card::COEUR));
+  hand.push_back(new Card(Card::DEUX, Card::COEUR));
+  hand.push_back(new Card(Card::TROIS, Card::COEUR));
+  hand.push_back(new Card(Card::QUATRE, Card::COEUR));
+  hand.push_back(new Card(Card::CINQ, Card::COEUR));
+
+  std::cout << "Debut des test (test_best_hand)." << std::endl;
+
+  if (!best_hand(hand) == 100000000)
+    std::cout << "Echec du 1er test." << std::endl;
+
+  for (Card *i : hand) delete i;
   std::cout << "Tout les test ont etais passe" << std::endl;
 }
 
@@ -156,6 +249,7 @@ int main() {
   }
   // display_hand(hand);
 
+  test_best_hand_sub_function();
   test_best_hand();
 
   // // Ne pas oublier de libere la memoire (uniquement si on fait "new ...")
