1e3b5f8697e4ac2ed02f91abdc8d470eda1b9812
Cesar Dablemont <cesardablemont@gmail.com>
2024-03-15 15:43:19 +0100

    update

--- display.hpp
+++ display.hpp
@@ -10,6 +10,6 @@
 void display_game(Game myGame);
 void display_hand(std::vector<Card *> hand);
 void display_unordered_map(const std::unordered_map<Card::VALUE, int> &uMap);
-
+std::string valeur_to_string(int valeur);
 
 #endif
